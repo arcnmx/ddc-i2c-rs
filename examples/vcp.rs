85a8f10b8e8424b7a9f7dab1d0abb24065c95570
arcnmx <arcnmx@users.noreply.github.com>
2018-03-27 19:41:07 -0400

    Fix mccs version example

--- examples/vcp.rs
+++ examples/vcp.rs
@@ -8,7 +8,7 @@ fn ddc<D: Ddc>(mut ddc: D) where
     D::Error: ::std::fmt::Debug,
 {
     let mccs_ver = ddc.get_vcp_feature(0xdf).expect("failed to read VCP value");
-    println!("MCCS version is {:04x}", mccs_ver.maximum());
+    println!("MCCS version is {:04x}", mccs_ver.value());
 
     let input = ddc.get_vcp_feature(0x60).expect("failed to read VCP value");
     println!("input is {:?}", input);
